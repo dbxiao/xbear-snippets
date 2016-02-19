8f29f3bb331ee765785b7b00658c6de39e7c1253
unknown <doublexiao@qq.com>
2016-02-19 16:18:46 +0800

    updata

--- index.js
+++ index.js
@@ -6,19 +6,132 @@
 
 'use strict';
 
-exports.name = 'xbear-snippets';
-exports.desc = 'text and file snippets tools';
+var $options = {};
+var $fs = require("fs");
+var $path = require("path");
 
-exports.register = function(commander) {
+function snippets(_options){
+    $options = {
+        snippetsType   : _options.snippetsType,   // 操作类型 [widget]
+        snippetsAction : _options.snippetsAction, // 操作时间 [add]
+        snippetsName   : _options.snippetsName,   // 文件名称 [wgt]
+        path           : _options.snippetsType +"/"+_options.snippetsName, // 文件路径 [widget/wgt]
 
-    commander
-        .option('-r, --root <path>', 'set snippets path')
-        .action(function(){
-            var argv = Array.prototype.slice.call(arguments);
-            var options = argv.pop();
-            var cmd = argv.shift();
-            console.log("-----------------------------");
-            console.log("-------xbear snippets--------");
-            console.log(cmd);
-        });
+        fileType       : ["html", "js", "css"]    // 操作文件属性
+    };
+
+    snippets.fn.existWidget();
 }
+
+/**
+ * [existWidget 检查当前模块是否存在widget]
+ * @return {[type]} [description]
+ */
+snippets.prototype = {
+
+    /**
+     * [existWidget description]
+     */
+    existWidget : function(){
+        var path = $options.path;
+
+        $fs.exists(path, function(res){
+            if(res == true){
+                console.log("[Error] this path:"+path+" exists");
+            }else{
+                snippets.fn.appFolder(path);
+            }
+        });
+    },
+
+    /**
+     * [appFolder 新建widget或者page目录]
+     * @param  {String} path [目录地址]
+     */
+    appFolder : function(path){
+        $fs.mkdirSync(path);
+        $fs.mkdirSync(path+"/images");
+        snippets.fn.addFile(path);
+    },
+
+    /**
+     * [writeFile 写入数据]
+     */
+    writeFile : function(_path, _type, _str){
+        var file = $options.path+"/"+_path+"."+_type;
+        $fs.appendFile(file, _str, 'utf8');
+    },
+
+    /**
+     * [extTpl description]
+     * @param  {String} files [文件数据]
+     * @param  {String} name  [文件后缀]
+     */
+    extTpl : function(files, name){
+        var reg = /(_STYLE_NAME_|_SCRIPT_NAME_)/gi;
+        var tpl = files.replace(reg, function(m,$1,$2){
+            if(m == "_STYLE_NAME_"){
+                return name;
+            }else if(m == "_SCRIPT_NAME_"){
+                return name;
+            }else{
+                return m;
+            }
+        });
+        return tpl;
+    },
+
+    /**
+     * [firstUpper 首字母大写]
+     * @param  {String} text [字符串]
+     */
+    firstUpper : function(text){
+        console.log(text);
+        text = text.replace(/(^|\s+)\w/g,function(s){
+            return s.toUpperCase();
+        });
+        console.log(text);
+        return text;
+    },
+
+    /**
+     * [addFile 增加配置文件]
+     * @param {[type]} _path [文件路径]
+     */
+    addFile : function(_path){
+        var str = "";
+        var x;
+        var path= $options.snippetsName;
+        
+
+        for(x in $options.fileType){
+            var files = $fs.readFileSync( $path.join(__dirname, "view/widget_tpl." + $options.fileType[x]), "utf8");
+
+            if($options.fileType[x] == "html" || $options.fileType[x] == "css"){
+                files = snippets.fn.extTpl(files, $options.snippetsName);
+            }else if($options.fileType[x] == "js"){
+                let fileName = $options.snippetsName.split("-");
+                for(let x in fileName){
+                    if(x == 0){
+                        continue;
+                    }else{
+                        fileName[x] = snippets.fn.firstUpper(fileName[x]);
+                    }
+                };
+                fileName = fileName.join("");
+                files = snippets.fn.extTpl(files, fileName);
+            }else{
+                return;
+            }
+
+            snippets.fn.writeFile(path, $options.fileType[x], files);
+        };
+
+        console.log("[Success] "+path+" is builded success!");
+    }
+};
+
+snippets.fn = snippets.prototype;
+
+
+module.exports = snippets;
---
+++ view/widget_tpl.js
@@ -0,0 +1,25 @@
+/**
+ * [script_name description]
+ */
+
+var _SCRIPT_NAME_ = function(_option){
+	this.option = {
+
+	};
+	$.extend(this.option, _option);
+	this.init();
+};
+
+_SCRIPT_NAME_.prototype = {
+	/** [init 初始化] */
+	init : function(){
+
+	},
+
+	/** [handle 事件绑定] */
+	handle : function(){
+
+	}
+}
+
+module.exports = _SCRIPT_NAME_;
